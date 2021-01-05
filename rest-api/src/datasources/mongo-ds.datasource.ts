db44790a392fb82e8d706bc3cb4cc4a7e737ff49
arkhurst <fiiarkhurst@gmail.com>
2021-01-05 15:07:40 +0000

    basic crud for user works

--- rest-api/src/datasources/mongo-ds.datasource.ts
+++ rest-api/src/datasources/mongo-ds.datasource.ts
@@ -4,13 +4,15 @@ import {juggler} from '@loopback/repository';
 const config = {
   name: 'mongoDS',
   connector: 'mongodb',
-  url: 'mongodb+srv://fiifi:<fiifi123>@cluster0.izolt.mongodb.net/<Test>?retryWrites=true&w=majority',
-  host: '',
-  port: 0,
-  user: '',
-  password: '',
-  database: '',
-  useNewUrlParser: true
+  url:
+    'mongodb+srv://fiifi:fiifi123@cluster0.izolt.mongodb.net/<Test>?retryWrites=true&w=majority',
+  // host: '',
+  // port: 0,
+  // user: 'fiifi',
+  // password: 'fiifi123',
+  // database: 'Test',
+  protocol: 'mongodb+srv',
+  useNewUrlParser: true,
 };
 
 // Observe application's life cycle to disconnect the datasource when
@@ -18,7 +20,8 @@ const config = {
 // gracefully. The `stop()` method is inherited from `juggler.DataSource`.
 // Learn more at https://loopback.io/doc/en/lb4/Life-cycle.html
 @lifeCycleObserver('datasource')
-export class MongoDsDataSource extends juggler.DataSource
+export class MongoDsDataSource
+  extends juggler.DataSource
   implements LifeCycleObserver {
   static dataSourceName = 'mongoDS';
   static readonly defaultConfig = config;
